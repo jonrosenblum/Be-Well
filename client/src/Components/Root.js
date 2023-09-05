515b91568ed535cf80f29498c85f9bb611e781b5
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-09-05 13:43:05 -0400

    Starting Routes/Adding first components for React app

---
+++ client/src/Components/Root.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export default function Root() {
+    return (
+        <div>
+            {/* <Header /> */}
+            <Outlet />
+            {/* <Footer /> */}
+        </div>
+    );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from './Components/Root.js';
+import Welcome from './Routes/Welcome.js';
+import Error from './Components/Error.js';
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Welcome />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
 reportWebVitals();
