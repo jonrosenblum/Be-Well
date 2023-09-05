eb556cc89a57e7e0ad5177850fd6d6c1e926d8a6
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-09-05 15:53:57 -0400

    BACK Button added for testing functionality to ROOT

--- client/src/Components/Static/Root.js
+++ client/src/Components/Static/Root.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 export default function Root() {
     return (
         <div>
+            <Link to="/"><button>Back Home</button></Link>
             <Outlet />
             <Footer />
         </div>
