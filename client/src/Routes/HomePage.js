a90a22a0dd6c5138f41bbd1b66321c4a7ecd4248
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-09-07 20:08:15 -0400

    welcome page skeleton

--- client/src/Routes/HomePage.js
+++ client/src/Routes/HomePage.js
@@ -1,13 +1,16 @@
 import HomePageNav from "../Components/Pieces/HomePageNav.js";
-import PatientsOrDoctorLogin from "../Components/Pieces/PatientsOrDoctorLogin.js";
-import "./Styles/HomePage.css"
-
+import Welcome from "../Components/Pieces/Welcome.js";
+import "./Styles/HomePage.css";
 
 export default function HomePage() {
     return (
         <div className="flex-container">
-            <HomePageNav />
-            <PatientsOrDoctorLogin />
+            <div className="nav-container">
+                <HomePageNav />
+            </div>
+            <div className="content-container">
+                <Welcome />
+            </div>
         </div>
-    )
+    );
 }
